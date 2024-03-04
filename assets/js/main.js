888a64859edbf3632276f7771c9990f19c9c365b
Prince Y <lightyombi2@gmail.com>
2024-03-04 17:15:46 +0100

    First commit

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+    /**
+     * Cette fonction permet d'effectuer un click sur le burger afin d'aaficher ou cacher le menu
+     */
+    $('.mobile-nav i').click(function(){
+
+        $('.site-nav-menu').toggleClass('mobile-menu')
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
